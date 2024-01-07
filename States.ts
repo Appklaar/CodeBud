5026dc8e6017302189f031e3572b6e74ececa24b
RNEvok <81921589+RNEvok@users.noreply.github.com>
2024-01-08 01:33:58 +0300

    States description updated

--- States.ts
+++ States.ts
@@ -3,8 +3,8 @@ import { CONFIG } from "./config";
 export const MODULE_STATES = {
   NOT_INITIATED: `This is default ${CONFIG.PRODUCT_NAME} state. ${CONFIG.PRODUCT_NAME} hasn't been initiated yet.`,
   INVALID_PARAMETERS: `Invalid ${CONFIG.PRODUCT_NAME} parameters were passed. Double check that provided apiKey is correct.`,
-  WORKING: `${CONFIG.PRODUCT_NAME} works as it should (DEVELOPMENT MODE).`,
-  WORKING_PRODUCTION: `${CONFIG.PRODUCT_NAME} works as it should (PRODUCTION MODE).`,
+  WORKING: `${CONFIG.PRODUCT_NAME} works correctly (DEVELOPMENT MODE).`,
+  WORKING_PRODUCTION: `${CONFIG.PRODUCT_NAME} works correctly (PRODUCTION MODE).`,
 };
 
 export type ModuleState = keyof typeof MODULE_STATES;
